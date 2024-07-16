a56609147ead4813104bb0c5468f57b1c625de9a
TheMelbine <melbineyt@gmail.com>
2024-07-16 15:47:29 +0300

    refactor networking module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,24 +1,17 @@
-{ config, lib, pkgs, ... }:
+{ config, ... }:
 
 {
-  imports =
-    [ 
+  imports = [ 
       ./hardware-configuration.nix
       ./modules/bundle.nix
       ./packages.nix
     ];
-
-  networking.hostName = "nixos"; 
-
+ 
   time.timeZone = "Europe/Moscow";
-
   i18n.defaultLocale = "en_US.UTF-8";
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   system.stateVersion = "23.11"; 
-
-  
 }
 
 
--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -1,3 +1,4 @@
 {
 networking.networkmanager.enable = true;  
+networking.hostName = "nixos"; 
 }

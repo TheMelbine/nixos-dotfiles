adeb7c2c62d75a401973284e86aafb22f96f6842
Melbine <melbineyt@gmail.com>
2024-07-27 18:57:37 +0300

    small refactor for workspaces

--- home-manager/modules/agsWidgets/ags/windows/bar/modules/workspacesModule.js
+++ home-manager/modules/agsWidgets/ags/windows/bar/modules/workspacesModule.js
@@ -4,22 +4,28 @@ export const WorkspacesModule = () => {
     const activeWorkspaceId = hyprland.active.workspace.bind("id");
     const workspaces = [1, 2, 3, 4, 5, 6, 7];
 
+    const BASE_CLASS = "workspaces__item";
+    const FOCUSED_CLASS = "workspaces__item--focused";
+    const OCCUPIED_CLASS = "workspaces__item--occupied";
+
+    const getClassNames = (activeId, id) => {
+        let className = BASE_CLASS;
+        if (activeId === id) {
+            className += ` ${FOCUSED_CLASS}`;
+        } else {
+            const windowsCount = (hyprland.getWorkspace(id)?.windows || 0);
+            if (windowsCount > 0) {
+                className += ` ${OCCUPIED_CLASS}`;
+            }
+        }
+        return className;
+    };
+
     const createWorkspaceButton = ({ id }) => {
         return Widget.Button({
             cursor: "pointer",
             on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-            className: activeWorkspaceId.as(activeId => {
-                let className = "workspaces__item";
-                if (activeId === id) {
-                    className += " workspaces__item--focused";
-                } else {
-                    const windowsCount = (hyprland.getWorkspace(id)?.windows || 0);
-                    if (windowsCount > 0) {
-                        className += " workspaces__item--occupied";
-                    }
-                }
-                return className;
-            }),
+            className: activeWorkspaceId.as(activeId => getClassNames(activeId, id)),
         });
     };
 
